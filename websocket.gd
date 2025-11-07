e7d8deb7c7d283abf08d634686b4e54452773b0d
Noah Lederer <lednah@protonmail.com>
2025-11-08 00:46:38 +0100

    Added Basic websocket
    Added new scene that is able to connect to our AWS WebSocket API

---
+++ websocket.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var websocket_url = "wss://kly6piqk82.execute-api.eu-north-1.amazonaws.com/development/"
+
+var socket : WebSocketPeer
+var current_state : WebSocketPeer.State = WebSocketPeer.STATE_CLOSED
+
+func _ready() -> void:
+	socket = WebSocketPeer.new()
+	socket.connect_to_url(websocket_url)
+	
+func _process(_delta: float) -> void:
+	socket.poll()
+	if socket.get_ready_state() != current_state:
+		current_state = socket.get_ready_state()
+		print("current state: ", current_state)
+	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			print("message received: ", JSON.stringify(socket.get_packet().get_string_from_utf8()))
