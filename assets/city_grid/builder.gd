a74a8e60c4c2aeca19dfa47491e81e2092ca9763
enr.a <enra94@gmail.com>
2025-11-08 15:39:50 +0100

    update(misc): changes

--- assets/city_grid/builder.gd
+++ assets/city_grid/builder.gd
@@ -1,11 +1,26 @@
 extends Node3D
 
 
-const MESH_OFFSET = Vector3(1, 0, 1)
+enum rotations {
+	NORTH = 16,
+	EAST = 0,
+	SOUTH = 22,
+	WEST = 14
+}
+
+enum modes {
+	BUILD,
+	DELETE
+}
+
 const CELL_SIZE = 2
+
+@export var mesh_offset = Vector3(1, 0, 1)
 
 var active_mesh: Mesh = null
 var active_mesh_idx: int = 0
+var rotation_counter: int = rotations.NORTH
+var mode: int = modes. BUILD
 
 @onready var grid_map: GridMap = $GridMap
 @onready var ghost_mesh: CSGMesh3D = $GhostMesh
@@ -15,6 +30,9 @@ var active_mesh_idx: int = 0
 func _ready() -> void:
 	active_mesh = grid_map.mesh_library.get_item_mesh(0)
 	hud.new_mesh_selected.connect(_select_new_mesh)
+	hud.rotate_plus_pressed.connect(_increment_rotation)
+	hud.rotate_minus_pressed.connect(_decrement_rotation)
+	hud.delete_pressed.connect(_enter_delete_mode)
 
 
 func _process(_delta: float) -> void:
@@ -23,15 +41,22 @@ func _process(_delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton
-	and event.button_index == MOUSE_BUTTON_LEFT
-	and event.is_pressed()
+	if (Input.is_action_just_pressed("place_object")
 	and not GameManager.placing_locked):
 		var cell_pos = _get_cell_coords()
-		grid_map.set_cell_item(cell_pos, active_mesh_idx)
+		match mode:
+			modes.BUILD:
+				grid_map.set_cell_item(cell_pos, active_mesh_idx, rotation_counter)
+			modes.DELETE:
+				grid_map.set_cell_item(cell_pos, GridMap.INVALID_CELL_ITEM, rotation_counter)
 	elif event is InputEventMouseMotion:
 		var cell_pos = _get_cell_coords()
-		ghost_mesh.position = cell_pos as Vector3 * CELL_SIZE + MESH_OFFSET
+		ghost_mesh.position = cell_pos as Vector3 * CELL_SIZE + mesh_offset
+	elif event.is_action_pressed("rotate_object_plus"):
+		_increment_rotation()
+	elif event.is_action_pressed("rotate_object_minus"):
+		_decrement_rotation()
+
 
 
 func _get_cell_coords():
@@ -64,9 +89,41 @@ func get_point_under_cursor() -> Vector3:
 	return hit_position  
 
 
+func _increment_rotation():
+	var key = (rotations.values().find(rotation_counter) + 1) % rotations.size()
+	rotation_counter = rotations.values()[key]
+
+
+func _decrement_rotation():
+	var key = (rotations.values().find(rotation_counter) - 1) % rotations.size()
+	rotation_counter = rotations.values()[key]
+
+
 func _select_new_mesh(idx: int) -> void:
 	print("new index: ", idx)
 	active_mesh_idx = idx
+
+
+func _enter_delete_mode():
+	if mode == modes.DELETE:
+		_enter_build_mode()
+		return
+	
+	mode = modes.DELETE
+	ghost_mesh.mesh.size.y = CELL_SIZE
+	ghost_mesh.material.albedo_color = Color.RED
+	mesh_offset.y = CELL_SIZE / 2
+
+
+func _enter_build_mode():
+	if mode == modes.BUILD:
+		return
+	
+	mode = modes.BUILD
+	ghost_mesh.mesh.size.y = 0.04
+	ghost_mesh.material.albedo_color = Color("b60051")
+	mesh_offset.y = 0
+
 
 
 func get_cursor_world_position() -> Vector3:
--- assets/ui/hud.gd
+++ assets/ui/hud.gd
@@ -2,18 +2,29 @@ class_name HUD
 extends Control
 
 signal new_mesh_selected(idx: int)
+signal rotate_plus_pressed
+signal rotate_minus_pressed
+signal delete_pressed()
 
-
-@onready var buttons: HBoxContainer = $Buttons
+@onready var buildings: HBoxContainer = $PanelContainer/Buttons/Buildings
+@onready var actions: HBoxContainer = $PanelContainer/Buttons/Actions
+@onready var button_rotate_plus: Button = $PanelContainer/Buttons/Actions/RotatePlus
+@onready var button_rotate_minus: Button = $PanelContainer/Buttons/Actions/RotateMinus
+@onready var button_delete: Button = $PanelContainer/Buttons/Actions/Delete
+@onready var panel_container: PanelContainer = $PanelContainer
 
 
 func _ready() -> void:
 	var counter: int = 0
-	for button: Button in buttons.get_children():
+	for button: Button in buildings.get_children():
 		button.pressed.connect(_on_button_pressed.bind(counter))
-		button.mouse_entered.connect(_lock_placing)
-		button.mouse_exited.connect(_unlock_placing)
 		counter += 1
+	
+	panel_container.mouse_entered.connect(_lock_placing)
+	panel_container.mouse_exited.connect(_unlock_placing)
+	button_rotate_plus.pressed.connect(func(): rotate_plus_pressed.emit())
+	button_rotate_minus.pressed.connect(func(): rotate_minus_pressed.emit())
+	button_delete.pressed.connect(_on_button_delete_pressed)
 
 
 func _on_button_pressed(idx: int):
@@ -21,8 +32,19 @@ func _on_button_pressed(idx: int):
 
 
 func _lock_placing():
+	print("locking")
 	GameManager.placing_locked = true
 
 
 func _unlock_placing():
+	print("unlocking")
 	GameManager.placing_locked = false
+
+
+func _on_button_delete_pressed():
+	if button_delete.modulate == Color.WHITE:
+		button_delete.modulate = Color.RED
+	else:
+		button_delete.modulate = Color.WHITE
+	
+	delete_pressed.emit()
