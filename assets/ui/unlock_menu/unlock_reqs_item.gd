58b85c4116ae4fd53c1058db75d23e6344cabe89
felifluid <coding@felifluid.de>
2025-11-09 11:14:53 +0100

    feat(unlocks): match tiles with unlock condition and categories

--- assets/ui/unlock_menu/unlock_reqs_item.gd
+++ assets/ui/unlock_menu/unlock_reqs_item.gd
@@ -2,8 +2,11 @@ class_name UnlockRequirementItem
 extends Resource
 
 @export var item_name: String
-@export var fire: int = 1
-@export var water: int = 1
-@export var earth: int = 1
-@export var air: int = 1
-@export var mystic: int = 1
+@export var category: String
+@export var preview: Texture2D
+@export var default: bool = false
+@export var fire: int = 0
+@export var water: int = 0
+@export var earth: int = 0
+@export var air: int = 0
+@export var mystic: int = 0
